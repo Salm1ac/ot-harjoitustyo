df86c8b5895675d593c9c0dcf559dd9558ec763c
Risto Miinalainen <risto.miinalainen@gmail.com>
2020-05-10 23:05:47 +0300

    Viimeistelyä

--- ReMorse/src/main/java/remorse/domain/WordGame.java
+++ ReMorse/src/main/java/remorse/domain/WordGame.java
@@ -6,24 +6,6 @@ import remorse.data.DatabaseHandler;
  * Luokka on PointGamen erikoistapaus, jossa kysytään sanoja.
  */
 public class WordGame extends PointGame {    
-
-    /**
-     * Konstruktori luo testausta varten uuden valesatunnaisen PointGamen, 
-     * jossa kysytään sanoja.
-     * @param parser Käytettävä parseri
-     * @param seed Satunnaislukugeneraattorin alkuarvo
-     * @see remorse.domain.PointGame
-     * @see remorse.domain.Parser
-     */
-    /*
-    public WordGame(Parser parser, long seed) {
-        super(parser, seed);
-        this.prompts = new String[] {
-            "kenttä", "potku", "kettu", "hissi", "katko", "takoi",
-            "rikos", "pirinä", "pentti", "heheh", "pihdit",
-            "koita", "tulos", "pommi", "passi", "lahti"}; 
-    }
-    */
     
     /**
      * Konstruktori luo uuden satunnaisen PointGamen, jossa kysytään sanoja.
